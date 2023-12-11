e20bd6037bff87e6ad5a5437e139b626a7187e1d
Aman-Sirmaur19 <amansirmaur190402@gmail.com>
2023-12-12 02:45:32 +0530

    commit

--- lib/widgets/transactions_list.dart
+++ lib/widgets/transactions_list.dart
@@ -10,7 +10,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 350,
+      height: 600,
       child: ListView.builder(
         itemBuilder: (ctx, index) {
           return Card(
